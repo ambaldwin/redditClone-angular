ccd6134138f3fc30aca20646fc43ccaa907377e3
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-26 10:01:53 -0600

    post creation and seeding works

--- app.js
+++ app.js
@@ -7,43 +7,55 @@ app.controller('mainController', function($scope) {
         title: 'Denver',
         author: 'Anna Baldwin',
         image: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/2006-03-26_Denver_Skyline_I-25_Speer.jpg',
-        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj'
+        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
+        votes: 3,
+        date: 'January 12, 2016',
+        comments: []
     }, {
         title: 'Kearney',
         author: 'Ben Fox',
         description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
-        image: 'http://www.fillmurray.com/460/300'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/7/75/GPRRAMKearney.jpg',
+        votes: -50,
+        date: 'January 12, 2016',
+        comments: []
     }, {
         title: 'Chicago',
         author: 'Laura Hranac',
         image: 'https://tctechcrunch2011.files.wordpress.com/2015/02/chicago.jpg',
-        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj'
+        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
+        votes: 2,
+        date: 'January 12, 2016',
+        comments: []
     }]
 
-    // $scope.votes = function(city) {
-    //   if (city.votes > 0) {
-    //     return 'positive';
-    //   } else if (city.votes < 0) {
-    //     return 'negative';
-    //   } else {
-    //     return '';
-    //   }
-    // }
+    $scope.votes = function(city) {
+      if (city.votes > 0) {
+        return 'positive';
+      } else if (city.votes < 0) {
+        return 'negative';
+      } else {
+        return '';
+      }
+    }
 
     $scope.addPost = function(city) {
+        $scope.city.date = moment().calendar();
+        $scope.city.votes = 0;
         $scope.city = {};
+        $scope.city.comments = [];
         $scope.citiesArray.push(city);
         $scope.userForm.$setPristine()
         console.log(city);
     }
 
 
-  //   $scope.upVote = function(city) {
-  //      city.votes += 1
-  //  }
-   //
-  //  $scope.downVote = function(city) {
-  //      city.votes -= 1
-  //  }
+    $scope.upVote = function(city) {
+       city.votes += 1
+   }
+
+   $scope.downVote = function(city) {
+       city.votes -= 1
+   }
 
 });
