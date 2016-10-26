88ac4a732558efe87ac7465d17051576f7d3be8a
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-26 12:12:20 -0600

    style and comment forms added, no functionality

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ app.controller('mainController', function($scope) {
         title: 'Denver',
         author: 'Anna Baldwin',
         image: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/2006-03-26_Denver_Skyline_I-25_Speer.jpg',
-        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
-        votes: 3,
-        date: 'July 12, 2016',
+        description: 'Denver, officially the City and County of Denver, is the capital and most populous municipality of the U.S. state of Colorado. Denver is located in the South Platte River Valley on the western edge of the High Plains just east of the Front Range of the Rocky Mountains. The Denver downtown district is located immediately east of the confluence of Cherry Creek with the South Platte River, approximately 12 mi (19 km) east of the foothills of the Rocky Mountains. Denver is nicknamed the Mile-High City.',
+        votes: 7,
+        date: 'Yesterday at 10:57 AM',
         comments: []
     }, {
         title: 'Kearney',
         author: 'Ben Fox',
-        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
+        description: 'Kearney is a city in and the county seat of Buffalo County, Nebraska, United States. The population was 30,787 at the 2010 census. It is home to the University of Nebraska at Kearney. The westward push of the railroad as the Civil War ended gave birth to the community.',
         image: 'https://upload.wikimedia.org/wikipedia/commons/7/75/GPRRAMKearney.jpg',
-        votes: -50,
-        date: 'February 12, 2016',
+        votes: -4,
+        date: 'Last Monday at 9:19 PM',
         comments: []
     }, {
         title: 'Chicago',
         author: 'Laura Hranac',
         image: 'https://tctechcrunch2011.files.wordpress.com/2015/02/chicago.jpg',
-        description: 'asd;fljas;dlfja;sldfj;alsdfj;lasdfj;lasdjf;lasdfj;ladsfj;lkadjsf;lkadjsf;lkj',
+        description: 'Chicago is the third-most populous city in the United States. With over 2.7 million residents, it is the most populous city in the state of Illinois and the Midwestern United States, and the county seat of Cook County. The Chicago metropolitan area, often referred to as Chicagoland, has nearly 10 million people and is the third-largest in the U.S.',
         votes: 2,
         date: 'January 1, 2016',
         comments: []
